3c62e5ede58a8decfbdb7a717ca0a7cb30d4c497
Ece Ozalp <eceiozalp@gmail.com>
2022-03-02 10:23:00 -0500

    fixed programData and type issues

--- components/live-data-model.tsx
+++ components/live-data-model.tsx
@@ -1,116 +1,125 @@
-import React, { useState, useEffect} from 'react';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { useQuery, gql } from "@apollo/client";
 import { LineChart } from "d3reactor";
 interface Stock {
-  symbol:string,
-  time:string,
-  price:number
+  symbol: string;
+  time: string;
+  price: number;
 }
 
+interface StockData {
+  close: number[];
+  symbol: string;
+  timestamp: number[];
+}
+
+enum StockIndex {
+  MSFT = 0,
+  GOOG = 1,
+  AAPL = 2,
+  FB = 3,
+  AMZN = 4,
+}
 
 const GET_LIVE_DATA = gql`
- {
+  {
     getLiveData {
       symbol
       timestamp
       close
     }
   }
-`
+`;
 
 export default function GetLiveData() {
-
-  const [symbol, setSymbol] = useState('')
+  const [symbol, setSymbol] = useState("");
   const router = useRouter();
-
   useEffect(() => {
-    const symbol =  router.query.symbol as string;
-    setSymbol(symbol)
+    const symbol = router.query.symbol as string;
+    setSymbol(symbol);
   }, []);
-  console.log('curr symbol',symbol)
+  console.log("curr symbol", symbol);
 
   const { loading, error, data } = useQuery(GET_LIVE_DATA, {
     pollInterval: 60000,
   });
   if (loading) return <div>Loading</div>;
-  if (error) return <div>{`Error! ${error}`}</div>
+  if (error) return <div>{`Error! ${error}`}</div>;
 
-  console.log('liveData',data)
-  
+  const currData: StockData[] = data.getLiveData;
+  console.log("liveData", currData);
 
-  
-
-function convertTime(timeStamp:number){
+  function convertTime(timeStamp: number) {
     let unix_timestamp = timeStamp;
-// Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-let date = new Date(unix_timestamp * 1000);
-// Hours part from the timestamp
-let hours = date.getHours();
-// Minutes part from the timestamp
-let minutes = "" + date.getMinutes();
-// Seconds part from the timestamp
-let seconds = "0" + date.getSeconds();
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    let date = new Date(unix_timestamp * 1000);
+    // Hours part from the timestamp
+    let hours = date.getHours();
+    // Minutes part from the timestamp
+    let minutes = "" + date.getMinutes();
+    // Seconds part from the timestamp
+    let seconds = "0" + date.getSeconds();
 
-// Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substring(-2) + ':' + seconds.substring(-2);
-  return formattedTime;
-
-
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ":" + minutes.substring(-2) + ":" + seconds.substring(-2);
+    return formattedTime;
   }
 
-function formattedData(time:any,price:any){
-let dataArr=[]  
-let stock:Stock={} as Stock
+  function formattedData(time: number[], price: number[]) {
+    let dataArr = [];
 
-for(let i=0;i<10;i++){
-  stock.time=convertTime(time[i]);
-  stock.symbol=symbol;
-  stock.price=price[i];
-  dataArr.push(stock);
-}
-return dataArr;
-}
-let programData:any=formattedData(data.getLiveData[0].timestamp,data.getLiveData[0].close)
-console.log(programData)
-const time1=convertTime(data.getLiveData[0].timestamp[5])
+    for (let i = 0; i < 10; i++) {
+      const stock: Stock = {} as Stock;
+      stock.time = convertTime(time[i]);
+      stock.symbol = symbol;
+      stock.price = price[i];
+      dataArr.push(stock);
+    }
+    return dataArr;
+  }
+  let programData: any = symbol
+    ? formattedData(
+        data.getLiveData[StockIndex[symbol as keyof typeof StockIndex]].timestamp,
+        data.getLiveData[StockIndex[symbol as keyof typeof StockIndex]].close
+      )
+    : undefined;
+  console.log("program data", programData);
+  const time1 = convertTime(data.getLiveData[0].timestamp[5]);
 
-// let programData:any=[
-//   { "Date": "2004-07-01", "language": "Java", "popularity": 30.37 },
-//   { "Date": "2004-08-01", "language": "Java", "popularity": 29.99 },
-//   {
-//     "Date": "2004-09-01",
-//     "language": "Java",
-//     "popularity": 29.709999999999997
-//   },
-//   { "Date": "2004-10-01", "language": "Java", "popularity": 29.12 },
-//   { "Date": "2004-11-01", "language": "Java", "popularity": 29.59 },
-//   {
-//     "Date": "2004-12-01",
-//     "language": "Java",
-//     "popularity": 29.759999999999998
-//   },
-//   { "Date": "2005-01-01", "language": symbol, "popularity": 29.68 },
-//   { "Date": "2005-02-01", "language": "Java", "popularity": 30.29 }]
+  // let programData:any=[
+  //   { "Date": "2004-07-01", "language": "Java", "popularity": 30.37 },
+  //   { "Date": "2004-08-01", "language": "Java", "popularity": 29.99 },
+  //   {
+  //     "Date": "2004-09-01",
+  //     "language": "Java",
+  //     "popularity": 29.709999999999997
+  //   },
+  //   { "Date": "2004-10-01", "language": "Java", "popularity": 29.12 },
+  //   { "Date": "2004-11-01", "language": "Java", "popularity": 29.59 },
+  //   {
+  //     "Date": "2004-12-01",
+  //     "language": "Java",
+  //     "popularity": 29.759999999999998
+  //   },
+  //   { "Date": "2005-01-01", "language": symbol, "popularity": 29.68 },
+  //   { "Date": "2005-02-01", "language": "Java", "popularity": 30.29 }]
 
   return (
     <>
-    <p>test time format:{time1}</p>
-    <LineChart
+      <p>test time format:{time1}</p>
+      {/* <LineChart
         data={programData}
         height={"90%"}
         width={"80%"}
-        xKey="Date"
-        yKey="popularity"
-        yAxisLabel="USD"
-        xGrid={true}
+        xKey='time'
+        yKey='price'
+        yAxisLabel='Price'
         yGrid={true}
-        legend="right"
-        groupBy="language"
-        colorScheme="schemeReds"
-      />
-    
+        legend='right'
+        colorScheme='schemeReds'
+      /> */}
     </>
   );
 }
--- components/symbolTable.tsx
+++ components/symbolTable.tsx
@@ -1,52 +1,44 @@
-import * as React from 'react';
-import Link from 'next/link'
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
-
+import * as React from "react";
+import Link from "next/link";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-  
-  
-  
- 
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-export default function SymbolTable({stocks}:any){
-  
-  const aapl=stocks.data.getStocks[2];
-  const msft=stocks.data.getStocks[0];
-  const amzn=stocks.data.getStocks[4];
-  const fb=stocks.data.getStocks[3];
-  const goog=stocks.data.getStocks[1];
-  console.log('fb',fb)
-  
-  function createData(symbol:string, price:number, change:string, changePercent:number,prev:number) {
-    
-    return { symbol, price, change,changePercent,prev };
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+export default function SymbolTable({ stocks }: any) {
+  const aapl = stocks.data.getStocks[2];
+  const msft = stocks.data.getStocks[0];
+  const amzn = stocks.data.getStocks[4];
+  const fb = stocks.data.getStocks[3];
+  const goog = stocks.data.getStocks[1];
+  console.log("fb", fb);
+
+  function createData(symbol: string, price: number, change: string, changePercent: number, prev: number) {
+    return { symbol, price, change, changePercent, prev };
   }
   // function changes(symbol:any){
   //   let change:number=(symbol.close[fb.close.length-2]-symbol.close[fb.close.length-1])/symbol.close[fb.close.length-1];
@@ -54,66 +46,69 @@ export default function SymbolTable({stocks}:any){
   //   return change
   // }
 
-  function rowData(symbol:any){
-    const obj:any={}
+  function rowData(symbol: any) {
+    const obj: any = {};
     obj.price = symbol.close.slice(-1);
-    let changeValue=Number((symbol.close[fb.close.length-1]-symbol.close[fb.close.length-2]).toFixed(2))
+    let changeValue = Number((symbol.close[fb.close.length - 1] - symbol.close[fb.close.length - 2]).toFixed(2));
     let change;
-    if(changeValue<0){
-      change="-$"+ changeValue.toString().slice(1)
-    }else{
-      change="+$"+ changeValue.toString().slice(1)
+    if (changeValue < 0) {
+      change = "-$" + changeValue.toString().slice(1);
+    } else {
+      change = "+$" + changeValue.toString().slice(1);
     }
-     obj.change = change
-    obj.changePercent = (100*(symbol.close[fb.close.length-1]-symbol.close[fb.close.length-2])/symbol.close[fb.close.length-1]).toFixed(3);
-    obj.prev = symbol.close[fb.close.length-2]; 
+    obj.change = change;
+    obj.changePercent = (
+      (100 * (symbol.close[fb.close.length - 1] - symbol.close[fb.close.length - 2])) /
+      symbol.close[fb.close.length - 1]
+    ).toFixed(3);
+    obj.prev = symbol.close[fb.close.length - 2];
 
     return obj;
   }
 
   //tickers' data
   const rows = [
-    createData('AAPL',rowData(aapl).price, rowData(aapl).change,rowData(aapl).changePercent,rowData(aapl).prev),
-    createData('MSFT', rowData(msft).price, rowData(msft).change, rowData(msft).changePercent,rowData(msft).prev),
-    createData('AMZN', rowData(amzn).price, rowData(amzn).change, rowData(amzn).changePercent,rowData(amzn).prev),
-    createData('FB', rowData(fb).price, rowData(fb).change, rowData(fb).changePercent,rowData(fb).prev),
-    createData('GOOG', rowData(goog).price, rowData(goog).change, rowData(goog).changePercent,rowData(goog).prev),
+    createData("AAPL", rowData(aapl).price, rowData(aapl).change, rowData(aapl).changePercent, rowData(aapl).prev),
+    createData("MSFT", rowData(msft).price, rowData(msft).change, rowData(msft).changePercent, rowData(msft).prev),
+    createData("AMZN", rowData(amzn).price, rowData(amzn).change, rowData(amzn).changePercent, rowData(amzn).prev),
+    createData("FB", rowData(fb).price, rowData(fb).change, rowData(fb).changePercent, rowData(fb).prev),
+    createData("GOOG", rowData(goog).price, rowData(goog).change, rowData(goog).changePercent, rowData(goog).prev),
   ];
-    return (
+  return (
     <>
-    <h1>I am rich!</h1>
-    <h1>{aapl.symbol}</h1>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 200 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Symbol</StyledTableCell>
-            <StyledTableCell align="left">Price</StyledTableCell>
-            <StyledTableCell align="left">Change</StyledTableCell>
-            <StyledTableCell align="left">Change %</StyledTableCell>
-            <StyledTableCell align="left">Prev Close</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.symbol}>
-              <StyledTableCell component="th" scope="row">
-                  <Link href={{ pathname: '/stock/', query: { symbol: row.symbol }}}>
-                  
-                    {row.symbol}
-                  </Link>
-                  
-              </StyledTableCell>
-              <StyledTableCell align="left">{row.price}</StyledTableCell>
-              <StyledTableCell className= {row.change >= 0 ?  'change1': 'change2'} align="left">{row.change}</StyledTableCell>
-              <StyledTableCell className= {row.change >= 0 ?  'change1': 'change2'} align="left">{row.changePercent}  %</StyledTableCell>
-              <StyledTableCell align="left">{row.prev}</StyledTableCell>
-              {/* <StyledTableCell align="left">{row.c}</StyledTableCell> */}
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <h1>I am rich!</h1>
+      <h1>{aapl.symbol}</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 200 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Symbol</StyledTableCell>
+              <StyledTableCell align='left'>Price</StyledTableCell>
+              <StyledTableCell align='left'>Change</StyledTableCell>
+              <StyledTableCell align='left'>Change %</StyledTableCell>
+              <StyledTableCell align='left'>Prev Close</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.symbol}>
+                <StyledTableCell component='th' scope='row'>
+                  <Link href={{ pathname: "/stock/", query: { symbol: row.symbol } }}>{row.symbol}</Link>
+                </StyledTableCell>
+                <StyledTableCell align='left'>{row.price}</StyledTableCell>
+                <StyledTableCell className={Number(row.change) >= 0 ? "change1" : "change2"} align='left'>
+                  {row.change}
+                </StyledTableCell>
+                <StyledTableCell className={Number(row.change) >= 0 ? "change1" : "change2"} align='left'>
+                  {row.changePercent} %
+                </StyledTableCell>
+                <StyledTableCell align='left'>{row.prev}</StyledTableCell>
+                {/* <StyledTableCell align="left">{row.c}</StyledTableCell> */}
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
-    )
+  );
 }
