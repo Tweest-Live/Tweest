9886f95c20509a2d91dd02056cfa37dcd2f6b33b
axie <axie0320@gmail.com>
2022-03-02 19:27:13 -0500

    updated navbar style and theme

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,39 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Link from "next/link"
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  },
+});
+
 export default function Navbar() {
+  console.log('theme:', darkTheme);
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href='/'>Tweest</Link>
-          </Typography>
-          <Link href='/'>Home</Link>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link href='/'>Tweest</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+    </ThemeProvider>
   );
 }
